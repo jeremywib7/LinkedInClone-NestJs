beae705a845efcb0aed5aef413056b6c6d5fe8e7
Jeremy Yonathan <jeremywib7@gmail.com>
2022-07-13 10:09:12 +0700

    work on feed post entity

--- src/feed/feed.module.ts
+++ src/feed/feed.module.ts
@@ -1,7 +1,12 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { FeedPostEntity } from './models/feed.post.entity';
 import { FeedService } from './services/feed.service';
 
 @Module({
+  imports: [
+    TypeOrmModule.forFeature([FeedPostEntity])
+  ],
   providers: [FeedService]
 })
 export class FeedModule {}
